b5fc49929ec3c5e185c5d9c6da4e76184689fcf5
hissue <wldmx@naver.com>
2023-12-19 22:56:40 +0900

    Feat: init

--- backend/src/main/java/com/laundry/backend/BackendApplication.java
+++ backend/src/main/java/com/laundry/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package org.africalib.gallery.backend;
+package com.laundry.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/laundry/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/laundry/backend/BackendApplicationTests.java
@@ -1,4 +1,4 @@
-package org.africalib.gallery.backend;
+package com.laundry.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
