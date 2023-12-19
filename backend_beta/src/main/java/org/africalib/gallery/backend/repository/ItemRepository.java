8d1a76acec50030cd5d5e9d4f481608039f7d078
SUHYUN JI <68486112+hissue@users.noreply.github.com>
2023-12-19 23:01:15 +0900

    Delete backend_beta directory

--- backend_beta/src/main/java/org/africalib/gallery/backend/repository/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package org.africalib.gallery.backend.repository;
-
-import org.africalib.gallery.backend.entity.Item;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-}
